3261a0b527668e21da5799021c9affdb06039331
imlynch666 <112213418+imlynch666@users.noreply.github.com>
2022-09-28 19:51:28 +0300

    Update main.py

--- 2 day/main.py
+++ 2 day/main.py
@@ -1,1 +1,14 @@
-#test2
+
+print('Tip calculator')
+
+total = float(input('What is your total bill: $'))
+
+split = float(input('How many people to split the bill: '))
+
+percents = float(input('How many percents to give? (10, 12, 15): '))
+
+pay = (total + ((total * percents)/100)) / split
+
+
+print('Every person should pay: ' + str(round(pay, 1)))
+
